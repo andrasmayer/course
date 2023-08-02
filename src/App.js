d292e06b4b0de48f4d4894bad6dbf656cd110715
unknown <mayerandras.weblap@gmail.com>
2023-08-02 13:22:35 +0200

    init project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Child from './components/child';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello Hacker</h1>
+      <h4>This is react</h4>
+      <Child name="Child"></Child>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/child.js
@@ -0,0 +1,7 @@
+const Child = (props)=>{
+    console.log(props)
+    return (<div>I am a {props.name} component</div>)
+
+}
+
+export default Child;
